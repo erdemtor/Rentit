972b8db77fe7bfd7ecaad05e08cb1493ffde720d
erdemtoraman <erdemtoraman93@gmail.com>
2017-05-13 14:09:08 +0300

    small changes on api outputs

--- rentit/src/main/java/com/rentit/inventory/rest/controller/InventoryRestController.java
+++ rentit/src/main/java/com/rentit/inventory/rest/controller/InventoryRestController.java
@@ -3,13 +3,16 @@ package com.rentit.inventory.rest.controller;
 import com.rentit.common.application.exceptions.PlantNotFoundException;
 import com.rentit.inventory.application.dto.PlantInventoryEntryDTO;
 import com.rentit.inventory.application.service.InventoryService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -35,10 +38,12 @@ public class InventoryRestController {
     }
 
     @GetMapping("/{id}/availability")
-    public boolean checkAvailability(@PathVariable String id,
+    public String checkAvailability(@PathVariable String id,
                                      @RequestParam(name = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate startDate,
                                      @RequestParam(name = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate endDate) throws PlantNotFoundException {
-        return inventoryService.isAvailable(id, startDate, endDate);
+        Map<String , Boolean> map = new HashMap<>();
+        map.put("availability",inventoryService.isAvailable(id, startDate, endDate));
+        return new JSONObject(map).toString();
     }
 
 
--- rentit/src/main/java/com/rentit/sales/web/controller/DashboardController.java
+++ rentit/src/main/java/com/rentit/sales/web/controller/DashboardController.java
@@ -10,7 +10,6 @@ import com.rentit.sales.domain.model.POStatus;
 import com.rentit.sales.web.dto.CatalogQueryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
