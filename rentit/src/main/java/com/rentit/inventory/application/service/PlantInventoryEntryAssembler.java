9ab5c069634ec00800e03b45373dae6b638d232e
erdemtoraman <erdemtoraman93@gmail.com>
2017-05-18 18:54:27 +0300

    ABV LINX

--- rentit/src/main/java/com/rentit/inventory/application/service/PlantInventoryEntryAssembler.java
+++ rentit/src/main/java/com/rentit/inventory/application/service/PlantInventoryEntryAssembler.java
@@ -1,14 +1,22 @@
 package com.rentit.inventory.application.service;
 
+import com.rentit.common.application.exceptions.PlantNotFoundException;
+import com.rentit.common.rest.ExtendedLink;
 import com.rentit.inventory.application.dto.PlantInventoryEntryDTO;
 import com.rentit.inventory.domain.model.PlantInventoryEntry;
 import com.rentit.inventory.rest.controller.InventoryRestController;
+import com.rentit.sales.rest.controller.SalesRestController;
+import org.eclipse.jetty.http.HttpMethod;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 import org.springframework.stereotype.Service;
+import retrofit.http.GET;
 
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Service
 public class PlantInventoryEntryAssembler extends ResourceAssemblerSupport<PlantInventoryEntry, PlantInventoryEntryDTO> {
@@ -18,6 +26,13 @@ public class PlantInventoryEntryAssembler extends ResourceAssemblerSupport<Plant
     @Override
     public PlantInventoryEntryDTO toResource(PlantInventoryEntry plantInventoryEntry) {
         PlantInventoryEntryDTO dto = createResourceWithId(plantInventoryEntry.getId(), plantInventoryEntry);
+        try {
+            dto.add(new ExtendedLink(
+                    ControllerLinkBuilder.linkTo(methodOn(InventoryRestController.class)
+                            .show(plantInventoryEntry.getId())).toString(), "self", org.springframework.http.HttpMethod.GET));
+        } catch (PlantNotFoundException e) {
+            e.printStackTrace();
+        }
         dto.set_id(plantInventoryEntry.getId());
         dto.setName(plantInventoryEntry.getName());
         dto.setDescription(plantInventoryEntry.getDescription());
