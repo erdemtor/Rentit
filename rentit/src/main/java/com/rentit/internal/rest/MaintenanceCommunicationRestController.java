2d8c99b3a1561bd7c99a34e1213cb3bc5a0e8559
erdemtoraman <erdemtoraman93@gmail.com>
2017-05-25 16:37:15 +0300

    *

--- rentit/src/main/java/com/rentit/internal/rest/MaintenanceCommunicationRestController.java
+++ rentit/src/main/java/com/rentit/internal/rest/MaintenanceCommunicationRestController.java
@@ -1,10 +1,14 @@
 package com.rentit.internal.rest;
 
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.rentit.common.domain.model.BusinessPeriod;
 import com.rentit.internal.service.InternalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import retrofit.http.GET;
 
 import java.time.LocalDate;
 
@@ -12,6 +16,7 @@ import java.time.LocalDate;
  * Created by erdem on 22.05.17.
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/api/maintenance")
 public class MaintenanceCommunicationRestController {
     @Autowired
@@ -22,6 +27,16 @@ public class MaintenanceCommunicationRestController {
                                        @RequestParam(name = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
                                        @RequestParam(name = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) throws Exception {
         return internalService.updatePurchaseOrdersAboutMaintenance(plantId, BusinessPeriod.of(startDate, endDate));
+    }
+
+    @PostMapping()
+    public String createMaintenanceTask(@RequestBody String body) throws UnirestException {
+        return Unirest.post("http://0.0.0.0:8888/api/maintenance").body(body).asString().getBody();
+    }
+
+    @GetMapping
+    public String getTasks() throws UnirestException {
+        return Unirest.get("http://0.0.0.0:8888/api/maintenance").asString().getBody();
     }
 
 }
